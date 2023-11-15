d89ec15fe22ae0f6254ce682cbfacd33d55f02da
Connor O'Brien <connorbobrien@gmail.com>
2023-11-15 12:19:07 -0300

    Add tailwind and daisy example

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,32 @@ import { servers } from "./data/servers";
 import TestAccordion from "./components/TestAccordion";
 // import { theme } from "./constants/theme";
 
+import { Button } from "react-daisyui";
+
 function App() {
 	return (
-		<ChakraProvider>
+		<div className="container mx-auto">
+			<img src="vite.svg" />
+			<article className="prose lg:prose-xl mx-auto">
+				<Button>Heelo</Button>
+				<h1>Garlic bread with cheese: What the science tells us</h1>
+				<p>
+					For years parents have espoused the health benefits of eating garlic bread with cheese to their children, with the food earning
+					such an iconic status in our culture that kids will often dress up as warm, cheesy loaf for Halloween.
+				</p>
+				<p>
+					But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases springing up around the country.
+				</p>
+			</article>
 			{/* <div className="card"></div> */}
 			{/* <Card /> */}
 			{/* <ToggleButton />
 			<BaseButton>Base button</BaseButton>
 			<PrimaryButton>Primary</PrimaryButton>
 			<SecondaryButton>Secondary</SecondaryButton> */}
-			<ServerList servers={servers} />
-			<TestAccordion />
-		</ChakraProvider>
+			{/* <ServerList servers={servers} />
+			<TestAccordion /> */}
+		</div>
 	);
 }
 
