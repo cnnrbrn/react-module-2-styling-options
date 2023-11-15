3b69a15056d06bef4563b4c089a61d4215c1751a
Connor O'Brien <connorbobrien@gmail.com>
2023-11-15 11:07:59 -0300

    Add Chakra example

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 // import Card from "./components/Card";
-import { ThemeProvider } from "styled-components";
-
+// import { ThemeProvider } from "styled-components";
+import { ChakraProvider } from "@chakra-ui/react";
 import ServerList from "./components/ServerList";
 // import ToggleButton from "./components/ToggleButton";
 // import { BaseButton, PrimaryButton, SecondaryButton } from "./components/ui/buttons";
 // import "./sass/index.scss";
 import { servers } from "./data/servers";
-import { theme } from "./constants/theme";
+import TestAccordion from "./components/TestAccordion";
+// import { theme } from "./constants/theme";
 
 function App() {
 	return (
-		<ThemeProvider theme={theme}>
+		<ChakraProvider>
 			{/* <div className="card"></div> */}
 			{/* <Card /> */}
 			{/* <ToggleButton />
@@ -18,7 +19,8 @@ function App() {
 			<PrimaryButton>Primary</PrimaryButton>
 			<SecondaryButton>Secondary</SecondaryButton> */}
 			<ServerList servers={servers} />
-		</ThemeProvider>
+			<TestAccordion />
+		</ChakraProvider>
 	);
 }
 
--- src/components/ToggleButton/ToggleButton.styles.jsx
+++ src/components/ToggleButton/ToggleButton.styles.jsx
@@ -1,11 +1,3 @@
 import styled from "styled-components";
 
-export const Button = styled.button`
-	border: 0;
-	background: none;
-	padding: 1rem;
-	font-size: 18px;
-	min-width: 200px;
-	color: ${(props) => (props.$isActive ? props.theme.colours.fontOn : props.theme.colours.fontOff)};
-	background-color: ${(props) => (props.$isActive ? props.theme.colours.on : props.theme.colours.off)};
-`;
+export const Button = styled.button``;
